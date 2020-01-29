05fe8d6aa37fa78304e5c6722f279233303f59f4
Jandir <jandir.habitzreuter@amazingIT.com.br>
2020-01-28 21:32:41 -0300

    Middleware e debugg

--- Modulo01/index.js
+++ Modulo01/index.js
@@ -17,8 +17,43 @@ server.use(express.json());
 
 const users = ['Jandir', 'Trycia', 'Miguel'];
 
+// Utilizando middlewares global
+server.use((req, res, next)=>{
+
+console.log(`Método: ${req.method}`);
+
+// Deixar executar as proximas rotas para não bloquear
+return next();
+});
+
+
+// Middleware local usado em rotas especificas
+
+function checkUserExists(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({error: 'Erro de cadastro'});
+    }
+
+    return next();
+}
+
+function checkUserInArray(req, res, next){
+   const user = users[req.params.index];
+
+    if(!user){
+        return res.status(400).json({error: 'Usuário não encontrado '});
+    }
+
+    //alterando a variavel req
+    req.user = user;
+
+    return next();
+}
+
+
+
 // Rota para listar todos os usuarios
-server.get('/users', (req, res)=>{
+server.get('/users', checkUserInArray, (req, res)=>{
 
 return res.json(users);
 })
@@ -26,23 +61,23 @@ return res.json(users);
 
 //Route Params - vai acessar o id pela rota
 //http://localhost:3000/users/1
-server.get('/users/:index', (req, res)=>{
+server.get('/users/:index',checkUserInArray, (req, res)=>{
 
 const { index } = req.params;
 // Vai enviar o texto/objeto para o cliente quando acessar a rota /teste
-return res.json(users[index]);
+return res.json(req.user);
 
 
 });
 
-server.post('/users', (req, res) =>{
+server.post('/users', checkUserExists, (req, res) =>{
 
 const { name } = req.body;
 users.push(name);
 return res.json(users);
 });
 
-server.delete('/users/:index', (req, res)=>{
+server.delete('/users/:index',checkUserInArray, (req, res)=>{
     const { index } = req.params; 
     users.splice(index,1);
 
@@ -50,7 +85,7 @@ server.delete('/users/:index', (req, res)=>{
 
 });
 
-server.put('/users/:index', (req, res)=>{
+server.put('/users/:index', checkUserExists, checkUserInArray, (req, res)=>{
     const { index } = req.params; 
     const { name } = req.body;
 
@@ -71,3 +106,8 @@ server.listen(3000);
 // Request = Conteudo em formato de Json que vem no corpo da
 //requisição (PUT ou POST) com as informações para criar 
 //um usuário, alterar um usuário.
+
+
+//Middleware = uma função que recebe os parametros req e res, entre outros
+// parametros e manipulam as informações. Ele vai ser chamado
+// independente da rota que esteja acessando
